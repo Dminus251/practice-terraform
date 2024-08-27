674d97c6ac014a8f6ee4ca0251a144bc79be76d8
Dminus251 <yyk0370@naver.com>
2024-08-27 21:29:28 +0900

    오타 수정

--- demo03-nat/main.tf
+++ demo03-nat/main.tf
@@ -1,49 +1,49 @@
-module "vpc" {
+module "vpc" { #VPC
   source = "./modules/t-aws-vpc"
 }
 
-module "public_subnet" {
+module "public_subnet" { #Public Subnet
   source = "./modules/t-aws-public_subnet"
   vpc-id = module.vpc.vpc-id
 }
 
-module "private_subnet" {
+module "private_subnet" { #Private Subnet
   source = "./modules/t-aws-private_subnet"
   vpc-id = module.vpc.vpc-id
 }
 
-module "igw" {
+module "igw" { #Internet Gateway
   source = "./modules/t-aws-igw"
   vpc-id = module.vpc.vpc-id
 }
 
-module "route_table-igw_to_vpc" {
+module "route_table-igw_to_vpc" { #Route Internet Traffic To IGW
   source = "./modules/t-aws-rt"
   vpc-id = module.vpc.vpc-id
   gateway-id = module.igw.igw-id
   rt-usage = "igw"
 }
 
-module "rta-internet_to_public_subnet" {
+module "rta-internet_to_public_subnet" { #Associate route_table-igw_to_vpc with Public Subnet
   source = "./modules/t-aws-rta"
   count = module.public_subnet.public_subnet-length #이만큼 반복해서 모듈 생성
   subnet-id = module.public_subnet.public_subnet-id[count.index]
   rt-id = module.route_table-igw_to_vpc.rt-id
 }
 
-module "eip" {
+module "eip" { #Elastic IP
   source = "./modules/t-aws-eip"
   count = module.private_subnet.private_subnet-length
 }
 
-module "nat" {
+module "nat" { #NAT Gateway
   source = "./modules/t-aws-nat"
   count = module.private_subnet.private_subnet-length
   eip-id = module.eip[count.index].eip-id
   subnet-id = module.public_subnet.public_subnet-id[count.index]  #nat는 public subnet에 위치해야 함
 }
 
-module "route_table-internet_to_nat" {
+module "route_table-internet_to_nat" { #Route Internet Traffic to NAT
   source = "./modules/t-aws-rt"
   count = module.private_subnet.private_subnet-length
   vpc-id = module.vpc.vpc-id
@@ -51,7 +51,7 @@ module "route_table-internet_to_nat" {
   rt-usage = "nat"
 }
 
-module "rta-internet_to_nat" {
+module "rta-internet_to_nat" { #Associate route_table-internet_to_nat with Private Subnet
   source = "./modules/t-aws-rta"
   count = module.private_subnet.private_subnet-length #이만큼 반복해서 생성
   subnet-id = module.private_subnet.private_subnet-id[count.index]
--- demo03-nat/modules/t-aws-nat/main.tf
+++ demo03-nat/modules/t-aws-nat/main.tf
@@ -1,13 +1,9 @@
 resource "aws_nat_gateway" "nat" {
  
   allocation_id = var.eip-id #eip의 id
-  subnet_id     = var.subnet-id#subnet id
+  subnet_id     = var.subnet-id #subnet id
 
   tags = {
     Name = var.nat-name
   }
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  #depends_on = [aws_internet_gateway.example]
 }

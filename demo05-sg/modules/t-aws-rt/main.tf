44f8c35524256e51f127240941f963d8174c0ceb
Dminus251 <yyk0370@naver.com>
2024-08-29 19:23:35 +0900

    sg 생성. 변수 이름 등 점검 필요

---
+++ demo05-sg/modules/t-aws-rt/main.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "internet" {
+  vpc_id = var.vpc-id
+
+  route { #route 1: 모든 트래픽을 igw로
+    cidr_block = "0.0.0.0/0" #from
+    gateway_id = var.gateway-id   #to
+  }
+
+  tags = {
+    Name = "prctice-rt-${var.rt-usage}"
+  }
+}
---
+++ demo05-sg/modules/t-aws-sg/main.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "sg" {
+  vpc_id = var.sg-vpc_id
+
+#  ingress {
+#    from_port       = var.ingress-from_port
+#    to_port         = var.ingress-to_port
+#    protocol        = var.ingress-protocol
+#    cidr_blocks	    = var.ingress-cidr_blocks
+#    security_groups = var.ingress-security_group
+#  }
+  ingress {
+    from_port       = var.ingress["from_port"]
+    to_port         = var.ingress["to_port"]
+    protocol        = var.ingress["protocol"]
+    cidr_blocks	    = var.ingress["cidr_blocks"]
+    security_groups = var.ingress["security_groups"]
+  }
+  #나가는 건 딱히 제한하지 않아도 될 듯..?
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  tags	= {
+    Name = var.sg-name
+  }
+}
+

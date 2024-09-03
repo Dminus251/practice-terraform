eb09f9685855a205a6e7ee1713edf2598da1ed41
Dminus251 <yyk0370@naver.com>
2024-09-03 18:55:59 +0900

    change launch template ami to eks optimized amazon linux 2023

--- demo07-ng/modules/t-aws-launch_template/vars.tf
+++ demo07-ng/modules/t-aws-launch_template/vars.tf
@@ -1,6 +1,8 @@
 variable "lt-image_id" {
   type = string
-  default = "ami-05d2438ca66594916" #ubuntu 22.04
+  #default = "ami-05d2438ca66594916" #ubuntu 22.04
+  #default = "ami-008d41dbe16db6778" #amazon linux 2023
+  default = "ami-04f3fb3944c844ddf" #eks optimized amazon linux 2023
 }
 
 variable "lt-instance_type" {

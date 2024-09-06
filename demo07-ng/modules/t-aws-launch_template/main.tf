420130814a123b43c92bf54f3065e81c292c293e
Dminus251 <yyk0370@naver.com>
2024-09-06 14:03:25 +0900

    launch temmplate userdata 가독성 향상

--- demo07-ng/modules/t-aws-launch_template/main.tf
+++ demo07-ng/modules/t-aws-launch_template/main.tf
@@ -3,8 +3,18 @@ resource "aws_launch_template" "example" {
   instance_type 	 = var.lt-instance_type
 
   vpc_security_group_ids = var.lt-sg
-  #user_data 		 = base64encode("#!/bin/bash\n/etc/eks/bootstrap.sh ${var.cluster-name}\n") 
   key_name 		 = var.lt-key_name
-  user_data = base64encode("#!/bin/bash\n/etc/eks/bootstrap.sh ${var.cluster-name}\nyum update -y && curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.2/2024-07-12/bin/linux/amd64/kubectl && chmod +x ./kubectl && mv ./kubectl /usr/local/bin/ && aws configure set aws_access_key_id ${var.aws_access_key_id} && aws configure set aws_secret_access_key ${var.aws_access_key_secret} && aws configure set region ${var.region} && aws eks update-kubeconfig --region ${var.region} --name ${var.cluster-name}")
+  user_data = base64encode(<<EOF
+	#!/bin/bash
+	/etc/eks/bootstrap.sh ${var.cluster-name}
+	yum update -y &&
+	curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.2/2024-07-12/bin/linux/amd64/kubectl &&
+	chmod +x ./kubectl &&
+	mv ./kubectl /usr/local/bin/ &&
+	aws configure set aws_access_key_id ${var.aws_access_key_id} &&
+	aws configure set aws_secret_access_key ${var.aws_access_key_secret} &&
+	aws configure set region ${var.region} &&
+	aws eks update-kubeconfig --region ${var.region} --name ${var.cluster-name}
+	EOF
+	)
 }
-

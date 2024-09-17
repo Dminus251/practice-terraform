63e48d2b78fdc5cb88d78701f8161985c2c84bac
Dminus251 <yyk0370@naver.com>
2024-09-17 22:28:15 +0900

    using nginx ingress controller

--- demo09-monitoring/helm.tf
+++ demo09-monitoring/helm.tf
@@ -1,64 +1,64 @@
 ################################# AWS-LOADBALANCER-CONTROLLER ################################# 
-resource "helm_release" "alb-ingress-controller"{
-  #count = 0
-  #depends_on = [module.eks-cluster, module.public_subnet, helm_release.cert-manager]
-  depends_on = [module.eks-cluster, module.public_subnet]
-  repository = "https://aws.github.io/eks-charts"
-  name = "aws-load-balancer-controller" #release name
-  chart = "aws-load-balancer-controller" #chart name
-  version = "1.8.2"
-  namespace = "kube-system"
-  
-
-  set {
-	name  = "clusterName"
-	value = module.eks-cluster.cluster-name
-  }
-
-  set {
-	name  = "region"
-	value = var.AWS_REGION
-  }
-
-  set {
-	name  = "vpcId"
-	value = module.vpc.vpc-id
-  }
-
-  set {
-	name  = "rbac.create"
-	value = "true" #if true, create and use RBAC resource
-  }
-
-  set {
-	name  = "serviceAccount.create"
-	value = "false"
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-
-  set {
-	name  = "createIngressClassResource"
-	value = "true"
-  }
-
-  set {
-    name  = "webhook.service.port"
-    value = "443"
-  }
-
-  set {
-    name  = "webhook.service.targetPort"
-    value = "9443"
-  }
+#resource "helm_release" "alb-ingress-controller"{
+#  #count = 0
+#  #depends_on = [module.eks-cluster, module.public_subnet, helm_release.cert-manager]
+#  depends_on = [module.eks-cluster, module.public_subnet]
+#  repository = "https://aws.github.io/eks-charts"
+#  name = "aws-load-balancer-controller" #release name
+#  chart = "aws-load-balancer-controller" #chart name
+#  version = "1.8.2"
+#  namespace = "kube-system"
+#  
+#
+#  set {
+#	name  = "clusterName"
+#	value = module.eks-cluster.cluster-name
+#  }
+#
+#  set {
+#	name  = "region"
+#	value = var.AWS_REGION
+#  }
+#
+#  set {
+#	name  = "vpcId"
+#	value = module.vpc.vpc-id
+#  }
+#
+#  set {
+#	name  = "rbac.create"
+#	value = "true" #if true, create and use RBAC resource
+#  }
+#
+#  set {
+#	name  = "serviceAccount.create"
+#	value = "false"
+#  }
+#
+#  set {
+#    name  = "serviceAccount.name"
+#    value = "aws-load-balancer-controller"
+#  }
+#
+#  set {
+#	name  = "createIngressClassResource"
+#	value = "true"
+#  }
+#
+#  set {
+#    name  = "webhook.service.port"
+#    value = "443"
+#  }
+#
+#  set {
+#    name  = "webhook.service.targetPort"
+#    value = "9443"
+#  }
 #  set {
 #    name = "enableCertManager"
 #   value = "true"
 #  }
-}
+#}
 
 ################################# CERT-MANAGER ################################# 
 #resource "helm_release" "cert-manager"{
@@ -95,10 +95,10 @@ resource "helm_release" "prometheus"{
     name  = "alertmanager.persistence.storageClass"
     value = "gp2"
   }
-  set {
-    name = "server.prefixURL"
-    value = "/prometheus"
-  }
+#  set {
+#    name = "server.prefixURL"
+#    value = "/prometheus"
+#  }
 }
 ################################# GRAFANA ################################# 
 #resource "helm_release" "grafana"{
